e764deddbbecc25e76079b39f9e9334c047917a9
SBA\Bang <superbang175@gmail.com>
2022-09-17 13:08:45 +0900

    캐릭터 시스템 추가

---
+++ Assets/Scripts/CharacterMove/CharacterMove.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+namespace SuraSang
+{
+    public enum ButtonActions
+    {
+        Run,
+        Jump,
+        Crouch,
+        Catch,
+    }
+
+    public class CharacterMove : MonoBehaviour
+    {
+        // TODO : 다른곳으로 옮기자
+        public float Gravity;
+        public float FallingGravityMultiplier;// 떨어질때의 중력
+        public float GravityLimit;
+        
+        public float RunMultiplier;
+        public float CrouchMultiplier;
+        public float Speed;
+
+        public float CoyoteTime;// 절벽 끝나고 플레이어가 공중에 있어도 일정 시간동안 점프 가능
+        public float VariableJumpTime;// 袁몄슧 ?꾨Ⅴ硫????щ씪媛??湲곕뒫
+        public float AirControl;// 怨듭쨷 ?쒖뼱 ?λ젰
+
+        public float JumpPower;
+
+        public Vector3 MoveDir { get; set; }
+
+
+        // 벡터를 넘겨주기 위해 move만 따로 처리
+        public UnityAction<Vector2> OnMove { get; set; }
+        private Dictionary<ButtonActions, UnityAction<bool>> _buttonEvents;
+
+        private CharacterMoveState _currentState;
+
+        private CharacterActions _inputActions;
+        private InputAction _moveInputAction;
+
+        public CharacterController Controller => _controller;
+        private CharacterController _controller;
+
+        private Transform _cameraTransform;
+
+        private void Awake()
+        {
+            _buttonEvents = new Dictionary<ButtonActions, UnityAction<bool>>();
+
+            _controller = GetComponent<CharacterController>();
+            _cameraTransform = Camera.main.transform;
+
+            SetInputActions();
+
+            ChangeState(new CharacterMoveFalling(this));
+        }
+
+
+        private void OnEnable()
+        {
+            _inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputActions.Disable();
+        }
+
+        private void Update()
+        {
+            var moveInput = _moveInputAction.ReadValue<Vector2>();
+
+            OnMove?.Invoke(moveInput);
+            _currentState.UpdateState();
+
+
+            _controller.Move(MoveDir * Time.deltaTime);
+        }
+
+        public void ChangeState(CharacterMoveState state)
+        {
+            if (_currentState != null)
+            {
+                Debug.Log($"상태 종료: {_currentState.GetType()}");
+                _currentState.ClearState();
+                _buttonEvents.Clear();
+                OnMove = null;
+            }
+            _currentState = state;
+            _currentState.InitializeState();
+            Debug.Log($"상태 시작: {state.GetType()}");
+        }
+
+        private void SetInputActions()
+        {
+            _inputActions = new global::CharacterActions();
+            _moveInputAction = _inputActions.Player.Move;
+
+            _inputActions.Player.Run.performed += (x) => GetAction(ButtonActions.Run)?.Invoke(true);
+            _inputActions.Player.Run.canceled += (x) => GetAction(ButtonActions.Run)?.Invoke(false);
+
+            _inputActions.Player.Jump.started += (x) => GetAction(ButtonActions.Jump)?.Invoke(true);
+            _inputActions.Player.Jump.canceled += (x) => GetAction(ButtonActions.Jump)?.Invoke(false);
+
+            _inputActions.Player.Crouch.performed += (x) => GetAction(ButtonActions.Crouch)?.Invoke(true);
+            _inputActions.Player.Crouch.canceled += (x) => GetAction(ButtonActions.Crouch)?.Invoke(false);
+
+            _inputActions.Player.Catch.performed += (x) => GetAction(ButtonActions.Catch)?.Invoke(true);
+            _inputActions.Player.Catch.canceled += (x) => GetAction(ButtonActions.Catch)?.Invoke(false);
+        }
+
+        private UnityAction<bool> GetAction(ButtonActions type)
+        {
+            if (_buttonEvents.TryGetValue(type, out var action))
+            {
+                return action;
+            }
+            return null;
+        }
+
+        public void SetAction(ButtonActions type, UnityAction<bool> action)
+        {
+            if (!_buttonEvents.ContainsKey(type))
+            {
+                _buttonEvents.Add(type, action);
+            }
+            else
+            {
+                _buttonEvents[type] = action;
+            }
+        }
+
+        public Vector3 InputToCameraSpace(Vector2 input)
+        {
+            var foward = _cameraTransform.forward;
+            var right = _cameraTransform.right;
+            foward.y = 0;
+            right.y = 0;
+            foward.Normalize();
+            right.Normalize();
+
+            var fowardReVerticalInput = input.y * foward;
+            var rightRelHorizontalInput = input.x * right;
+
+            return fowardReVerticalInput + rightRelHorizontalInput;
+        }
+
+        public void LookVector(Vector3 dir)
+        {
+            dir.y = 0;
+            transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
+        }
+
+        public void Crouch(bool active)
+        {
+            _controller.height = active ? 1 : 2;
+        }
+    }
+}
