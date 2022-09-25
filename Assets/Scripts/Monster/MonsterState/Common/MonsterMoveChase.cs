b6065649f277def92262e12fc2bb961ab6f03d38
Super-Ba <superbang175@gmail.com>
2022-09-26 06:18:56 +0900

    몬스터 무브 스테이트 추가

---
+++ Assets/Scripts/Monster/MonsterState/Common/MonsterMoveChase.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuraSang
+{
+    public class MonsterMoveChase : MonsterMoveState
+    {
+        public MonsterMoveChase(CharacterMove characterMove) : base(characterMove) { }
+
+        public override void InitializeState() { }
+
+        public override void UpdateState()
+        {
+            _agent.SetDestination(Vector3.zero);
+        }
+
+        public override void ClearState() { }
+    }
+}
