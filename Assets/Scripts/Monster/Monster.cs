03a20088317133c78e5afe5d2f1d5360752c8044
Super-Ba <superbang175@gmail.com>
2022-09-26 06:19:14 +0900

    몬스터 fsm 적용

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.PlayerLoop;
 
 public enum Emotion
 {
     Happiness,
     Sadness,
     Anger,
-    Sleep,
     Default,
 }
 
 namespace SuraSang
 {
-    public abstract class Monster : MonoBehaviour
+    public abstract class Monster : CharacterMove
     {
-        public abstract Emotion getEmotion();
+        public NavMeshAgent Agent { get; protected set; }
+
+        public abstract Emotion Emotion { get; }
+        
+        public bool IsSleep { get; private set; } = false;
+
+        public void Absorbed()
+        {
+            ChangeState(new MonsterMoveSleep(this));
+        }
     }
 }
