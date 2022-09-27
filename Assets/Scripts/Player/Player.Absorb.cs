75b95495e9897bc29b74772f87d2d6a731d8400d
RuuNee <bio8617@gmail.com>
2022-09-27 18:04:31 +0900

    흡수와 기쁨스킬 로직 수정

--- Assets/Scripts/Player/Player.Absorb.cs
+++ Assets/Scripts/Player/Player.Absorb.cs
@@ -97,10 +97,14 @@ namespace SuraSang
             {
                 for (int i = 0; i < _hitTargetContainer.Count; i++)
                 {
-                    _isAbsorb = true;
-                    _timer = 0;
-                    CurrentEmotion = _hitTargetContainer[i].gameObject.GetComponent<Monster>().Emotion;
-                    gameObject.GetComponent<Renderer>().material = _hitTargetContainer[i].GetComponent<Renderer>().material;
+                    if (!_hitTargetContainer[i].gameObject.GetComponent<Monster>().IsSleep)
+                    {
+                        _isAbsorb = true;
+                        _timer = 0;
+                        gameObject.GetComponent<Renderer>().material = _hitTargetContainer[i].GetComponent<Renderer>().material;
+                        CurrentEmotion = _hitTargetContainer[i].gameObject.GetComponent<Monster>().Emotion;
+                        _hitTargetContainer[i].gameObject.GetComponent<Monster>().Absorbed();
+                    }
                 }
             }
         }
