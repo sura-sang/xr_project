3b5a263874d16aa1cd4a29b337000096fa0f918c
RuuNee <bio8617@gmail.com>
2022-09-30 18:53:31 +0900

    기쁨이 이동범위 태그 추가

--- Assets/Scripts/Monster/Happiness.cs
+++ Assets/Scripts/Monster/Happiness.cs
@@ -8,13 +8,13 @@ namespace SuraSang
     public class Happiness : Monster
     {
         public override Emotion Emotion => Emotion.Happiness;
-        
+
         private void Awake()
         {
             Agent = GetComponent<NavMeshAgent>();
             PlayerTransform = GameObject.Find("PlayerDummy").GetComponent<Transform>();
 
-            ChangeState(new MonsterMoveChase(this));
+            ChangeState(new HappinessIdle(this));
         }
     }
 }
