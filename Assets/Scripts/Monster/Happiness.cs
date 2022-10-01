1eb88eb4671a65d26ff7c29e3d82148efe847b95
RuuNee <bio8617@gmail.com>
2022-10-01 17:37:52 +0900

    코드 수정

--- Assets/Scripts/Monster/Happiness.cs
+++ Assets/Scripts/Monster/Happiness.cs
@@ -22,7 +22,9 @@ namespace SuraSang
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.yellow;
-            Gizmos.DrawLine(transform.position, _moveRange.transform.position);
+
+            if (_moveRange != null)
+                Gizmos.DrawLine(transform.position, _moveRange.transform.position);
         }
 
         private void FineNearestTag()
