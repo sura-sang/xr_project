c50778587cfefb7e5bc813ad15451bc0d3f08452
RuuNee <bio8617@gmail.com>
2022-10-04 16:48:30 +0900

    갓과 몸통 구분하는 태그 추가

--- Assets/Scripts/PuzzleSystem/ObserverFresh.cs
+++ Assets/Scripts/PuzzleSystem/ObserverFresh.cs
@@ -6,14 +6,37 @@ namespace SuraSang
 {
     public class ObserverFresh : PuzzleObserver
     {
+        public float Size = 3f;
+        public float GrowSpeed = 1f;
+        private float _time;
+        private Vector3 _originScale;
+
         private void Start()
         {
+            _originScale = transform.localScale;
             PuzzleManager.Instance.AddObserver(this);
         }
 
         public override void OnNotify()
         {
+            StartCoroutine(GrowUp());
             Debug.Log("갓버섯 퍼즐 실행");
+        }
+
+        IEnumerator GrowUp()
+        {
+            while (transform.localScale.x < Size)
+            {
+                transform.localScale = _originScale * (1f + _time * GrowSpeed);
+                _time += Time.deltaTime;
+
+                if (transform.localScale.x >= Size)
+                {
+                    _time = 0;
+                    break;
+                }
+                yield return null;
+            }
         }
     }
 }
