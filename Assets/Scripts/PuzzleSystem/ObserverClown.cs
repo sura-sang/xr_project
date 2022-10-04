3ceaab591a4330f9bd6cf4ca7039e7820dc6df76
RuuNee <bio8617@gmail.com>
2022-10-04 17:25:45 +0900

    광대 버섯 커지는 기능

--- Assets/Scripts/PuzzleSystem/ObserverClown.cs
+++ Assets/Scripts/PuzzleSystem/ObserverClown.cs
@@ -6,14 +6,39 @@ namespace SuraSang
 {
     public class ObserverClown : PuzzleObserver
     {
+        public float Size = 5f;
+        public float GrowSpeed = 1f;
+        private float _time;
+        public Vector3 _upSize;
+        private Vector3 _originScale;
+
         private void Start()
         {
+            _originScale = transform.Find("Cap").localScale;
             PuzzleManager.Instance.AddObserver(this);
         }
 
         public override void OnNotify()
         {
+            StartCoroutine(GrowUp());
             Debug.Log("광대 버섯 퍼즐 실행");
+        }
+
+        IEnumerator GrowUp()
+        {
+            while (transform.Find("Cap").localScale.x < Size)
+            {
+                transform.Find("Cap").localScale = _upSize * (1f + _time * GrowSpeed);
+
+                _time += Time.deltaTime;
+
+                if (transform.Find("Cap").localScale.x >= Size)
+                {
+                    _time = 0;
+                    break;
+                }
+                yield return null;
+            }
         }
     }
 }
