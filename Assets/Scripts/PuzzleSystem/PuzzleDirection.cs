7065fd5db895a2e2acd5630d1fbee2be11ca7424
SBA\Bang <superbang175@gmail.com>
2022-10-10 05:03:54 +0900

    방향을 사용하는 퍼즐 로직 베이스

---
+++ Assets/Scripts/PuzzleSystem/PuzzleDirection.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuraSang
+{
+    public class PuzzleContextDirection : PuzzleContext
+    {
+        public Vector3 Dir;
+
+        PuzzleContextDirection(Vector2 dir)
+        {
+            Dir = dir;
+        }
+    }
+
+
+    public abstract class PuzzleDirection : PuzzleElements
+    {
+        public float StartAngle;
+        public int DirCount;
+
+        private PuzzleContextDirection _context;
+
+
+        public override void OnNotify(PuzzleContext context)
+        {
+            _context = context as PuzzleContextDirection;
+        }
+
+
+        protected Vector3 GetVector(float radians)
+        {
+            return new Vector3(Mathf.Cos(radians), 0, Mathf.Sin(radians));
+        }
+
+        protected float GetAngle(Vector3 start, Vector3 end)
+        {
+            Vector3 dir = end - start;
+            dir.y = 0;
+            return Mathf.Atan2(dir.z, dir.x);
+        }
+
+        protected float GetNearestAngle(float angle)
+        {
+            var plusAngle = 360f / DirCount;
+
+            if(angle < 0)
+            {
+                angle += 360f;
+            }
+
+            for (int i = 0; i < DirCount; i++)
+            {
+                var result = StartAngle + i * plusAngle;
+                if (Mathf.Abs(result - angle) < plusAngle * 0.5f)
+                {
+                    return result;
+                }
+            }
+
+            return StartAngle;
+        }
+
+        protected void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+
+            var plusAngle = 360f / DirCount;
+
+            for (int i = 0; i < DirCount; i++)
+            {
+                var dir = GetVector((StartAngle + i * plusAngle) * Mathf.Deg2Rad).normalized * 3;
+                Gizmos.DrawLine(transform.position, transform.position + dir);
+            }
+        }
+    }
+}
