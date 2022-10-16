351e085ace68f2b1673dda6a4be5c9a4ae649f85
RuuNee <bio8617@gmail.com>
2022-10-16 15:33:07 +0900

    PuzzleContext에 Emotion과 IsNotify 추가

--- Assets/Scripts/PuzzleSystem/PuzzleElements.cs
+++ Assets/Scripts/PuzzleSystem/PuzzleElements.cs
@@ -6,11 +6,17 @@ namespace SuraSang
 {
     public class PuzzleContext
     {
-        
+        public Emotion SkillEmotion;
+
+        public PuzzleContext(Emotion emotion)
+        {
+            SkillEmotion = emotion;
+        }
     }
     
     public abstract class PuzzleElements : MonoBehaviour
     {
+        public bool IsNotify = false;
         //상태 update 메서드
         public abstract void OnNotify(PuzzleContext context);
     }
