abb036db2b91dc950cab8e4551c22ffdf3fa7041
RuuNee <bio8617@gmail.com>
2022-11-01 22:46:10 +0900

    오타 수정

--- Assets/Scripts/PuzzleSystem/Grow/PuzzleFresh.cs
+++ Assets/Scripts/PuzzleSystem/Grow/PuzzleFresh.cs
@@ -10,7 +10,7 @@ namespace SuraSang
         public float Size = 3f;
         public float GrowSpeed = 1f;
         private float _time; 
-        public BoxCollider platform;
+        public BoxCollider Platform;
 
         [SerializeField]
         private Transform _body;
@@ -18,14 +18,14 @@ namespace SuraSang
         private void Start()
         {
             PuzzleManager.Instance.AddObserver(this);
-            platform.enabled = false;
+            Platform.enabled = false;
         }
 
         public override void OnNotify(PuzzleContext context)
         {
             if (context.SkillEmotion == Emotion.Sadness && !IsNotify)
             {
-                platform.enabled = true;
+                Platform.enabled = true;
                 IsNotify = true;
                 _body.transform.DOMoveY(Size, GrowSpeed, false);
                 Debug.Log("갓버섯 퍼즐 실행");
