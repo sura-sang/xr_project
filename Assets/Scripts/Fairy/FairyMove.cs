c7eeafc6065c7333637ff5972f3cc7cae909296b
RuuNee <bio8617@gmail.com>
2022-11-07 23:24:38 +0900

    더미 정령 추가 + 플레이어 주변 회전

---
+++ Assets/Scripts/Fairy/FairyMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuraSang
+{
+    public class FairyMove : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private float _yPos;
+        [SerializeField] private float _radius = 1.0f;
+        [SerializeField] private float _angularVelocity = 100.0f;
+        [SerializeField] private float _angle = 0.0f;
+
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+            _yPos = _target.transform.position.y;
+            _angle += _angularVelocity * Time.deltaTime;
+            var offset = Quaternion.Euler(0.0f, _angle, 0.0f) * new Vector3(0.0f, 0.0f, _radius);
+            transform.position = new Vector3(_target.transform.position.x, _yPos, _target.transform.position.z) + offset;
+        }
+    }
+}
