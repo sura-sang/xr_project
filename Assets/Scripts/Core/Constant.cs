b9e11e553984ffe2632432406c5e310be6a8f00f
RuuNee <bio8617@gmail.com>
2022-11-15 03:41:15 +0900

    흡수 버그 수정

--- Assets/Scripts/Core/Constant.cs
+++ Assets/Scripts/Core/Constant.cs
@@ -7,15 +7,13 @@ namespace SuraSang
     public static class Constant
     {
         // 리소스 경로
-
-        public const string AngerAbsorbEffectPath = "Effect/Prefab/PREffect_ang/E_MS_common_AB_PREffect_ang_0922";
-        public const string HappyAbsorbEffectPath = "Effect/Prefab/PREffect_joy/E_MS_common_AB_PREffect_joy_0922";
-        public const string SadAbsorbEffectPath = "Effect/Prefab/PREffect_sad/E_MS_common_AB_PREffect_sad_0922";
-
+        public const string AngerAbsorbEffectPath = "Effect/Prefab/PREffect_sad/E_P_PR_AB";
+        public const string HappyAbsorbEffectPath = "Effect/Prefab/PREffect_sad/E_P_PR_AB";
+        public const string SadAbsorbEffectPath = "Effect/Prefab/PREffect_sad/E_P_PR_AB";
 
         public const string AngerTransEffectPath = "Effect/Prefab/PREffect_ang/E_P_ang_trans";
-        public const string HappyTransEffectPath = "Effect/Prefab/PREffect_joy/PREffect_P_joy_TRANS0924";
-        public const string SadTransEffectPath = "Effect/Prefab/PREffect_sad/E_TRANS_PREffect_sad_0924";
+        public const string HappyTransEffectPath = "Effect/Prefab/PREffect_joy/E_P_PR_joy_TRANS";
+        public const string SadTransEffectPath = "Effect/Prefab/PREffect_sad/E_P_PR_SAD_TRANS";
 
         public const string HappySkillEffectPath = "Effect/Prefab/PREffect_joy/PREffect_joy_SKILL1009";
     }
