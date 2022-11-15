db8ebb53ac502f73301224f553676189eadcaddd
RuuNee <bio8617@gmail.com>
2022-11-16 03:06:10 +0900

    달리면서 흡수 안되는 버그 임시 수정

--- Assets/Scripts/Player/PlayerState/PlayerMoveGrounded.cs
+++ Assets/Scripts/Player/PlayerState/PlayerMoveGrounded.cs
@@ -58,6 +58,14 @@ namespace SuraSang
                 _player.canMove();
             }
 
+            // TO DO : 나중에 수정해야 될지도?
+            // (강제로 Grounded 스테이트에서 CanMove가 false일 경우 Change 트리거 실행 시켜줌)
+            if(!_player.CanMove)
+            {
+                _player.Animator.SetTrigger("Change");
+                _player.Animator.SetBool("IsRunning", false);
+                _player.Animator.SetBool("IsWalking", false);
+            }
         }
 
         public override void ClearState() { }
@@ -65,9 +73,13 @@ namespace SuraSang
 
         private void OnRun(bool isOn)
         {
-            _isRunning = isOn;
-            if (isOn && _player.CurrentEmotion != Emotion.Sadness) _player.Animator.SetBool("IsRunning", true);
-            else _player.Animator.SetBool("IsRunning", false);
+            if (isOn && _player.CurrentEmotion != Emotion.Sadness && _player.CanMove)
+            {
+                _isRunning = isOn;
+                _player.Animator.SetBool("IsRunning", true);
+            }
+            else
+                _player.Animator.SetBool("IsRunning", false);
         }
 
         private void OnJump(bool isOn)
@@ -98,7 +110,7 @@ namespace SuraSang
             dir.y = _controller.isGrounded ? -1 : _player.MoveDir.y - _player.PlayerData.Gravity * Time.deltaTime;
             _player.MoveDir = dir;
 
-            if (input != Vector2.zero) _player.Animator.SetBool("IsWalking", true);
+            if (input != Vector2.zero && _player.CanMove) _player.Animator.SetBool("IsWalking", true);
             else _player.Animator.SetBool("IsWalking", false);
         }
 
