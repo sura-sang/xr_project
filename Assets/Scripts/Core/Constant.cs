baf34ca224435eefbdedddd76de039b999a7b07f
RuuNee <bio8617@gmail.com>
2022-11-15 18:48:51 +0900

    이펙트 리소스 경로 추가

--- Assets/Scripts/Core/Constant.cs
+++ Assets/Scripts/Core/Constant.cs
@@ -7,14 +7,52 @@ namespace SuraSang
     public static class Constant
     {
         // 리소스 경로
-        public const string AngerAbsorbEffectPath = "Effect/Prefab/PREffect_sad/E_P_PR_AB";
-        public const string HappyAbsorbEffectPath = "Effect/Prefab/PREffect_sad/E_P_PR_AB";
-        public const string SadAbsorbEffectPath = "Effect/Prefab/PREffect_sad/E_P_PR_AB";
 
-        public const string AngerTransEffectPath = "Effect/Prefab/PREffect_ang/E_P_ang_trans";
-        public const string HappyTransEffectPath = "Effect/Prefab/PREffect_joy/E_P_PR_joy_TRANS";
-        public const string SadTransEffectPath = "Effect/Prefab/PREffect_sad/E_P_PR_SAD_TRANS";
+        //흡수 이펙트
+        public const string AngerAbsorbEffectPath = "Effect/Prefab/PREffect_sad/E_P_PR_AB"; //O
+        public const string HappyAbsorbEffectPath = "Effect/Prefab/PREffect_sad/E_P_PR_AB"; //O
+        public const string SadAbsorbEffectPath = "Effect/Prefab/PREffect_sad/E_P_PR_AB"; //O
 
-        public const string HappySkillEffectPath = "Effect/Prefab/PREffect_joy/E_P_PR_joy_SKILL";
+        //변신 이펙트
+        public const string AngerTransEffectPath = "Effect/Prefab/PREffect_ang/E_P_ang_trans"; //O
+        public const string HappyTransEffectPath = "Effect/Prefab/PREffect_joy/E_P_PR_joy_TRANS"; //O
+        public const string SadTransEffectPath = "Effect/Prefab/PREffect_sad/E_P_PR_SAD_TRANS"; //O
+
+        //스킬 이펙트
+        public const string HappySkillEffectPath = "Effect/Prefab/PREffect_joy/E_P_PR_joy_SKILL"; //O
+        public const string AngerSkillRushEffectPath = "Effect/Prefab/PREffect_ang/E_P_ang_SKILL1";
+        public const string AngerSkillCrashEffectPath = "Effect/Prefab/PREffect_ang/E_P_ang_SKILL2";
+
+        //정령 이펙트
+        public const string AngerSoulEffect = "Effect/Prefab/PREffect_ang/E_ang_soul";
+        public const string HappySoulEffect = "Effect/Prefab/PREffect_ang/E_PR_Joy_soul";
+        public const string SadSoulEffect = "Effect/Prefab/PREffect_ang/E_P_PR_SAD_SOUL";
+
+        //분노 헤롱헤롱 이펙트 
+        public const string AngerDizzyEffect = "Effect/Prefab/PREffect_ang/E_MS_ang_STAR";
+
+        //IDLE 이펙트
+        public const string AngerIdleEffect = "Effect/Prefab/PREffect_ang/E_MS_ang_idle";
+        public const string HappyIdleEffect = "Effect/Prefab/PREffect_ang/E_P_PR_joy_IDLE";
+
+        //Walk 이펙트
+        public const string AngerWalkEffect = "Effect/Prefab/PREffect_ang/E_MS_ang_dust";
+        public const string HappyWalkEffect = "Effect/Prefab/PREffect_ang/E_MS_PR_joy_WALK";
+        public const string HappyWalk2Effect = "Effect/Prefab/PREffect_ang/E_P_PR_joy_WALK";
+
+        //Run 이펙트
+        public const string AngerRunEffect = "Effect/Prefab/PREffect_ang/E_MS_ang_dust";
+        public const string HappyRunEffect = "Effect/Prefab/PREffect_ang/E_MS_PR_joy_RUN";
+
+        //슬픔 몬스터가 기쁜 스킬에 들어갔을 때 쓰는 이펙트 (둘이 한세트)
+        public const string HappyFloorAuraEffect = "Effect/Prefab/PREffect_ang/E_MS_PR_FloorAura";
+        public const string HappyMuEffect = "Effect/Prefab/PREffect_ang/E_MS_PR_MU";
+
+        //기쁨 몬스터 스마일?
+        public const string HappySmileEffect = "Effect/Prefab/PREffect_ang/E_MS_PR_Joy_Smile";
+
+        //???뭔지 모르겠는 이펙트
+        public const string SadPoolEffect = "Effect/Prefab/PREffect_ang/E_MS_PR_SAD_POOL";
+        public const string SadPool2Effect = "Effect/Prefab/PREffect_ang/E_P_PR_SAD_POOL";
     }
 }
