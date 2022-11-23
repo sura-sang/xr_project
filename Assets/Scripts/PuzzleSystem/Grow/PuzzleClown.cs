5965b2dadc05ec52234c4e180017061ec43b982e
SBA\Bang <superbang175@gmail.com>
2022-11-24 03:03:31 +0900

    트램폴린 버섯 커지기 전에 사용할 수 있는 문제 수정

--- Assets/Scripts/PuzzleSystem/Grow/PuzzleClown.cs
+++ Assets/Scripts/PuzzleSystem/Grow/PuzzleClown.cs
@@ -18,6 +18,7 @@ namespace SuraSang
         private void Start()
         {
             PuzzleManager.Instance.AddObserver(this);
+            _trampolin.Enable = false;
         }
 
         public override void OnNotify(PuzzleContext context)
@@ -27,6 +28,8 @@ namespace SuraSang
                 IsNotify = true;
                 _hat.transform.DOScale(new Vector3(Size, Size, Size), GrowSpeed);
                 Debug.Log("광대 버섯 퍼즐 실행");
+
+                _trampolin.Enable = true;
             }
         }
     }
